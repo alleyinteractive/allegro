782a8a4b8a9fe145c1b819d54d40a468d04b8132
Greg Marshall <greg@alley.co>
2025-10-30 17:02:55 -0500

    a few updaets to customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,25 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use \App\Models\Customer;
 
+/**
+ * CustomerController
+ */
 class CustomerController extends Controller
 {
+    /**
+     * Validates the email of a customer. Adds it to the database if it doesn't exist.
+     * Determines if the email is verified or not.
+     *
+     * @param Request $request The api request.
+     * @return void
+     */
     public function validateEmail(Request $request) {
         $customer = Customer::where('email', $request->input('email'))->first();
         if ($customer) {
+            if ( ! empty( $request->input('name') ) && $customer->name !== $request->input('name') ) {
+                $customer->name = $request->input('name');
+                $customer->save();
+            }
             if ($customer->email_verified) {
                 // We'll return the email hashed here as well.
                 return response()->json(['message' => 'Email already verified.'], 200);
             }
-            // Send verification email logic would go here.
+            // Resend verification email.
+            $this->sendEmailVerification($customer);
             return response()->json(['message' => 'Verification email resent.'], 401);
         }
 
         // Add customer.
         $customer = Customer::create([
             'email' => $request->input('email'),
-            'name' => '',
+            'name' => $request->input('name') ?? '',
         ]);
-        // Send verification email logic would go here.
+        // Send verification email.
+        $this->sendEmailVerification($customer);
+
         return response()->json(['message' => 'Verification email sent.'], 401);
+    }
+
+    public function sendEmailVerification(Customer $customer) {
+        // Logic to send email verification.
     }
 }
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
     protected $fillable = [
         'name',
